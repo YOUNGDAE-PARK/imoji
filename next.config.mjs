01dd1e184ab4dbc081460f8f589a74e2d07d5274
ydpark <ydjava@gmail.com>
2026-05-24 10:00:40 +0900

    Switch from Firebase Hosting to App Hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-
-  images: {
-    unoptimized: true,
-  },
-
   experimental: {
     serverComponentsExternalPackages: ["@google/genai", "gifenc", "jszip"]
   }
